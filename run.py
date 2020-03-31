383dcc231146dc1d2277a5c18c2a6c9103fd73ee
puneeth <theflair.tk@gmail.com>
2020-03-31 22:17:17 +0530

    Setting up intitially

--- run.py
+++
@@ -1,8 +0,0 @@
-from flaskblog import dev_network_mesh
-import os
-
-PORT = int(os.environ.get('PORT', '8443'))
-app = dev_network_mesh()
-
-if __name__ == '__main__':
-    app.run( debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
